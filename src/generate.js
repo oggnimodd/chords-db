c4a37b9f397f7401cd8ba1e0407b569f7d49d62c
oggnimodd <arnold.ds.dev@gmail.com>
2025-03-11 09:35:45 +0000

    fix: force create lib folder before running generation

--- src/generate.js
+++ src/generate.js
@@ -1,15 +1,15 @@
-import { generate } from './tools';
-import db from './db';
-import fs from 'fs';
-import path from 'path';
+import { generate } from "./tools";
+import db from "./db";
+import fs from "fs";
+import path from "path";
 
 const createDirIfNeeded = () =>
-  fs.existsSync(path.join(__dirname, '..', 'lib')) ||
-  fs.mkdirSync(path.join(__dirname, '..', 'lib'));
+  fs.existsSync(path.join(__dirname, "..", "lib")) ||
+  fs.mkdirSync(path.join(__dirname, "..", "lib"));
 
 const generateJSON = (instrument) =>
   fs.writeFileSync(
-    path.join(__dirname, '..', 'lib', `${instrument}.json`),
+    path.join(__dirname, "..", "lib", `${instrument}.json`),
     JSON.stringify(generate(db[instrument]))
   );
 
@@ -33,7 +33,7 @@ const getNumberOfChords = (chords) =>
 
 const generateIndex = (db) => {
   fs.writeFileSync(
-    path.join(__dirname, '..', 'lib', `instruments.json`),
+    path.join(__dirname, "..", "lib", `instruments.json`),
     JSON.stringify(
       Object.assign(
         ...Object.keys(db).map((instrument) => ({
@@ -54,6 +54,7 @@ const processCommand = (json) =>
       Object.keys(db).map((instrument) => generateJSON(instrument))
     : console.log(prettyObjectToJSON(getInstrumentsDB()));
 
-const json = process.argv.length > 2 && process.argv[2] === 'json';
+const json = process.argv.length > 2 && process.argv[2] === "json";
 
+createDirIfNeeded();
 processCommand(json);
